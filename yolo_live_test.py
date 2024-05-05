78e148ac2de24ac5ce738fd4adec45757de638c0
Yusuf ÖZEN <ozenyusuf3549@gmail.com>
2024-05-05 16:09:15 +0300

    test

---
+++ yolo_live_test.py
@@ -0,0 +1,23 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("models/best_yolov8.pt")
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    
+    success, frame = cap.read()
+
+    if success:
+        results = model(frame)        
+        annotated_frame = results[0].plot()        
+        cv2.imshow('YOLOv8 Inference', annotated_frame)
+        
+        if cv2.waitKey(1) == 13:  
+            break
+    else:
+        
+        break
+
+cap.release()
+cv2.destroyAllWindows()
